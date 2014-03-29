d93c7155dcbea563890bb2217bf92d709faee52d
Mark Kenneth Ranosa <Ken@Marks-MacBook-Pro.local>
2014-03-30 06:35:16 +0800

    Disabled employees call, its breaking everything

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,7 +2,7 @@
 
 angular.module('timeClockChallegeApp')
   .controller('MainCtrl', function ($scope, $http) {
-    $http.get('/api/employees').success(function(result) {
-      console.log(result);
-    });
+    // $http.get('/api/employees').success(function(result) {
+    //   console.log(result);
+    // });
   });

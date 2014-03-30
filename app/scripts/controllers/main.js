f08d96f52decd47d87ff6c7b72fc38268d060609
Mark Kenneth Ranosa <Ken@Marks-MacBook-Pro.local>
2014-03-30 19:06:29 +0800

    Forced user to wait for all employee data to be loaded, before he can proceed with clocking in

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,4 +3,26 @@
 angular.module('timeClockChallegeApp')
 	.controller('MainCtrl', function($scope, $http, EmployeeService) {
 
+		$scope.loading = true;
+		$scope.loadingMessage = "Loading employees info please wait...";
+
+		$scope.safeApply = function(fn) {
+			var phase = this.$root.$$phase;
+			if (phase == '$apply' || phase == '$digest')
+				this.$eval(fn);
+			else
+				this.$apply(fn);
+		};
+		
+		$scope.$on('update-loading-message', function (event, message){
+			$scope.safeApply(function() {
+				$scope.loadingMessage = message;	
+			});
+		});
+
+		$scope.$on('hide-loading', function (event, param){
+			$scope.safeApply(function() {
+				$scope.loading = false;
+			});
+		});
 	});
--- app/scripts/services/employee_service.js
+++ app/scripts/services/employee_service.js
@@ -1,8 +1,9 @@
 'use strict';
 
 angular.module('timeClockChallegeApp')
-	.service('EmployeeService', function EmployeeService($http) {
+	.service('EmployeeService', function EmployeeService($http, $rootScope) {
 		var employees = [];
+		var currCount = 0;
 		var EmployeeService = function() {
 			$http.get('/api/employees').success(function(result) {
 				var employeesData = result.employees;
@@ -15,8 +16,14 @@ angular.module('timeClockChallegeApp')
 				console.log(arrayLength);
 				for (var i = 0; i < arrayLength; i++) {
 					$http.get('/api/employee?id=' + employeesData[i].id + '&token=' + token).success(function(result) {
-						console.log('Added : ' + result);
-						employees.push(result);
+						console.log(result.employee);
+						employees.push(result.employee);
+						currCount += 1;
+						$rootScope.$broadcast('update-loading-message', 'loaded ' + currCount + ' of ' + arrayLength);
+
+						if(currCount === arrayLength){
+							$rootScope.$broadcast('hide-loading');
+						}
 					});
 				}
 			});
